a772aa2f6a163075741934122677a9708df23c6a
jiexins <jasonmayday@gmail.com>
2026-06-26 15:42:42 -0400

    Update path.py

--- python_scripts/core/path.py
+++ python_scripts/core/path.py
@@ -25,6 +25,10 @@ import os
 import getpass
 
 
+def get_repo_root():
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+
+
 
 def get_base_paths():
     # Get the username and home directory
@@ -32,16 +36,19 @@ def get_base_paths():
     
     homedir = os.path.expanduser('~')
     
+    env_path = os.environ.get('ZEOLITE_SOLVATION_PATH')
+    if env_path:
+        return os.path.abspath(os.path.expanduser(env_path))
 
     # Set the base path based on the username and home directory
     if username == 'shi.1909' and homedir == r'C:\Users\shi.1909':    # Windows
-        base_path = r"C:\Users\shi.1909\OneDrive - The Ohio State University\GitHub\zeolite_ml_project"
+        base_path = r"C:\Users\shi.1909\OneDrive - The Ohio State University\GitHub\3dcnn-zeolite-solvation"
     elif username == 'jiexin' and homedir == '/Users/jiexin':          # Mac OS
-        base_path = "/Users/jiexin/Library/CloudStorage/OneDrive-TheOhioStateUniversity/GitHub/zeolite_ml_project"
+        base_path = "/Users/jiexin/Library/CloudStorage/OneDrive-Personal/GitHub/3dcnn-zeolite-solvation"
     elif username == 'jiexins' and homedir == '/users/PAS2536/jiexins':         # Linux
-        base_path = "/fs/ess/PAS2536/jiexins/zeolite_ml_project"
+        base_path = "/fs/ess/PAS2536/jiexins/3dcnn-zeolite-solvation"
     else:
-        raise ValueError("Unsupported user or home directory")
+        base_path = get_repo_root()
     
     return base_path
 
@@ -50,15 +57,7 @@ def get_base_paths():
 
 def get_paths(path):
     
-    # Set the base path based on the operating system
-    if sys.platform.startswith('linux'):
-        base_path = "/fs/ess/PAS2536/jiexins/zeolite_ml_project"
-    elif sys.platform.startswith('darwin'):
-        base_path = "/Users/jiexin/Library/CloudStorage/OneDrive-TheOhioStateUniversity/GitHub/zeolite_ml_project"
-    elif sys.platform.startswith('win'):
-        base_path = r"C:\Users\shi.1909\OneDrive - The Ohio State University\GitHub\zeolite_ml_project"
-    else:
-        raise ValueError("Unsupported operating system")
+    base_path = get_base_paths()
 
     # Define the paths
     path_dic = {
