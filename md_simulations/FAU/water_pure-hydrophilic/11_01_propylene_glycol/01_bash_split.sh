c6a188b75ced17d94a48fc2b447f17b7ec49034a
jiexins <jasonmayday@gmail.com>
2026-06-27 03:09:46 -0400

    1

---
+++ md_simulations/FAU/water_pure-hydrophilic/11_01_propylene_glycol/01_bash_split.sh
@@ -0,0 +1,57 @@
+#/bin/bash 
+
+cutoff=7
+adsorbate_file=11_01_propylene_glycol
+
+cp ../../fep_generate/${adsorbate_file}/01_geo_opt/00-OUTPUT/*converged.xyz ./
+cp ../../fep_generate/${adsorbate_file}/21_npt_nvt/{dump_nvt_samp.lammpsdata.lammpstrj,data_nvt_samp.lammpsdata} ./
+cp /zfs/curium/xiutinc/zeolite/FAU/intE_dft_one_unitcell/03_01_1_3_propanediol/{02_bash_g_sp.sh,03_bash_traj_intE_calc.sh,calc_intE.py,traj_2_intE.py,zeolite_ad_g.py} ./
+
+cwd="$(pwd)" 
+
+job_name_aq_ad_geo="adl$(basename $PWD)"
+job_name_aq_zeo="zl$(basename $PWD)"
+
+python zeolite_ad_g.py *converged.xyz converged_one_unit.xyz
+
+
+cwd="$(pwd)" 
+
+######################## 1. split trajectory to 10 xyz file ########################
+sed -i 's/\r$//' *.lammpstrj
+number_lines=`expr $(sed -n 4p  *.lammpstrj) + 9`
+split -l ${number_lines} -d --additional-suffix=.traj *.lammpstrj intE
+rm *00.traj
+
+sed -i 's/\r$//' *converged.xyz
+gas_xyz=`expr $(sed -n 1p  converged_one_unit.xyz) + 1`
+
+
+######################## 2.dft simulation for each frame in aqueous phase #############################
+### for each traj make xyz_zeo_ad_aq and xyz_zeo_aq in aqueous phase
+for traj_file in *.traj; do
+	cd ${cwd}
+	echo $traj_file
+	foldername=$(basename ${traj_file} .traj)
+	mkdir -p ${foldername}/{01_${foldername}_aq,02_${foldername}_aq_zeo}
+	python traj_2_intE.py $traj_file converged_one_unit.xyz $cutoff
+	
+	mv $traj_file ${foldername}/
+	mv ${foldername}_aq.xyz ${foldername}/01_${foldername}_aq
+	mv ${foldername}_aq_zeo.xyz ${foldername}/02_${foldername}_aq_zeo
+	
+	### adsorbate relax in aqueous phase
+	cd ${foldername}/01_${foldername}_aq
+	cp /home/xiutinc/DFT/31_intE_dft_one_unit/01_intE_aq/* ./
+	sed -i "s/GAS_XYZ/${gas_xyz}/" *.inp
+	sed -i "s/aq_ad_geo/${job_name_aq_ad_geo}/" sub_aq.sh
+	qsub sub_aq.sh 
+	
+	### single point of zeolite reference in aqueous phase
+	cd ../02_${foldername}_aq_zeo
+	cp /home/xiutinc/DFT/31_intE_dft_one_unit/02_intE_aq_zeo/* ./
+	sed -i "s/aq_zeo_sp/${job_name_aq_zeo}/" sub_aq_zeo.sh
+	qsub sub_aq_zeo.sh
+	
+done >> ./traj_intE.txt
+	
---
+++ md_simulations/FAU/water_pure-hydrophilic/11_01_propylene_glycol/intE05/03_intE05_g_sp/sub_ad_g_sp.sh
@@ -0,0 +1,90 @@
+#/bin/bash 
+#PBS -N adg11_01_propylene_glycol
+#PBS -l select=1:ncpus=20:mpiprocs=20:mem=125gb:interconnect=fdr,walltime=72:00:00
+#PBS -q work1 
+#PBS -j oe
+#PBS -m n
+#PBS -M xiutinc@g.clemson.edu
+
+echo ''
+echo ' # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # '
+echo ''
+echo ' STARTING THE CALCULATION!'
+echo ''
+echo ' # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # '
+echo '' 
+
+
+qstat -xf $PBS_JOBID 
+
+cd $PBS_O_WORKDIR
+
+cwd="$(pwd)"
+
+
+
+# file needed for valence density simulation
+
+
+
+# rename xyz file and modify inp file
+cd ${cwd}
+filename=$(basename *.xyz .xyz)
+atom_number=$(head -n 1 *.xyz)
+mv *.inp ${filename}.inp
+sed -i "s/ADSORBATE_NAME/${filename}/" *.inp
+sed -i "s/ADSORBATE_NUMBER/${atom_number}/" *.inp
+
+
+# Starting the CP2K calculation 
+cd ${cwd}
+module purge
+
+export MODULEPATH=/software/ModuleFiles/modules/linux-centos8-ivybridge:$MODULEPATH
+module load cp2k/9.1-gcc/9.5.0-mpi-openmp 
+
+# export MODULEPATH=/software/ModuleFiles/modules/linux-centos8-ivybridge:$MODULEPATH
+# module load cp2k/7.1-gcc/8.3.1-mpi
+
+module list 
+export OMP_NUM_THREADS=1
+
+# Creating the CP2K scratch directory
+SCRATCH_DIR="/scratch1/$USER/CP2K-${PBS_JOBID}/"
+mkdir -p ${SCRATCH_DIR}
+cp ./* ${SCRATCH_DIR}/.
+echo ${SCRATCH_DIR} > ./zb-TO-SCRATCH.dir
+echo ./ > ${SCRATCH_DIR}/zb-TO-PBS-WORKDIR.dir
+echo '' > ./zc-${PBS_JOBID%.*}.JOBID
+ 
+# Running CP2K
+cd ${SCRATCH_DIR}
+NAME=$(basename *.inp .inp)
+mpirun -n 20 cp2k.popt -i ${NAME}.inp -o ${NAME}.out
+
+# Copying over all the raw output files from CP2K
+cd ${cwd}
+mkdir -p ./00-OUTPUT 
+cp ${SCRATCH_DIR}/* ./00-OUTPUT
+if [[ -e ./00-OUTPUT/zb-TO-PBS-WORKDIR.dir ]]; then 
+	rm ./00-OUTPUT/zb-TO-PBS-WORKDIR.dir
+fi  
+
+# Creating converged structure .xyz file 
+# cd ${cwd}/00-OUTPUT
+# file_xyz=$(find . -type f -name "*pos-1.xyz")
+# number_atoms=`expr $(head -1 ${file_xyz}) + 2`
+# tail -${number_atoms} ${file_xyz} >> ${file_xyz%.*}_converged.xyz
+# echo ${file_xyz%.*}
+
+ENDING VALENCE DENSITY CALCULATION
+echo ''
+echo ' # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # '
+echo ''
+echo ' ENDING THE VALENCE DENSITY CALCULATION!'
+echo ''
+echo ' # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # '
+echo ''
+
+qstat -xf $PBS_JOBID
+rm -f core.*
